4d2921c53b2d67293f2aa82110c627775932a931
Mike Nunan <mike_nunan@hotmail.com>
2022-01-16 00:49:52 +0000

    refactor: tidy up

--- FirstWordle.fs
+++ FirstWordle.fs
@@ -2,21 +2,17 @@
 open System
 open System.IO
 
-// Code from https://fsharpforfunandprofit.com/posts/fvsc-download/
+let fetchUrl readHtml url =
+    let request = WebRequest.Create(Uri(url))
+    use response = request.GetResponse()
+    use stream = response.GetResponseStream()
+    use reader = new StreamReader(stream)
+    readHtml reader url
 
-// Fetch the contents of a web page
-let fetchUrl callback url =
-    let req = WebRequest.Create(Uri(url))
-    use resp = req.GetResponse()
-    use stream = resp.GetResponseStream()
-    use reader = new IO.StreamReader(stream)
-    callback reader url
-
-let myCallback (reader:IO.StreamReader) url = 
+let readHtml (reader:StreamReader) url = 
     let html = reader.ReadToEnd()
     let html1000 = html.Substring(0,1000)
     printfn "Downloaded %s. First 1000 is %s" url html1000
-    html      // return all the html
+    html
 
-//test
-let dictionary = fetchUrl myCallback "https://raw.githubusercontent.com/wooorm/dictionaries/main/dictionaries/en/index.dic"
+let dictionary = fetchUrl readHtml "https://raw.githubusercontent.com/wooorm/dictionaries/main/dictionaries/en/index.dic"
